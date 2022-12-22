569e7a8d41288b9ccc715ea8d4dd96f363481517
BuntinMA_Home <BuntinMA@mail.ru>
2022-12-22 22:42:28 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+class laptop {
+    public double mem;
+    public double hdd;
+    public double screen; //диагональ экрана
+    public String brand;
+    public String model;
+    public String cpu;
+    public double cpuspeed;
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
