b152ef19ab325f80bea34b7007c2274f46e88cac
BuntinMA_Home <BuntinMA@mail.ru>
2022-12-22 22:52:00 +0300

    Начал работу с методами - сделал первый метод printLaptop.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,17 @@ class laptop {
     public String model;
     public String cpu;
     public double cpuspeed;
+    public void printlLaptop() {
+        System.out.println("Brand: " + brand + ", model: " + model);
+        System.out.println("CPU: " + cpu + cpuspeed + "GHz");
+        System.out.println("Memory: " + mem + "Gb");
+        System.out.println("HDD: " + hdd + "Gb");
+        System.out.println("Screen: " + screen + '"');
+    }
+
+    public void addLaptop() {
+        System.out.println("Screen: " + screen + '"');
+    }
 }
 public class Main {
     public static void main(String[] args) {
