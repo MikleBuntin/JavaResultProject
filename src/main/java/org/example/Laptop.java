07aa0f75182d55c45485f5feb3b5742b7e9d2bc5
MikleBuntin <BuntinMA@mail.ru>
2023-02-27 20:48:13 +0300

    Всё сначала...

--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -2,40 +2,72 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Laptop {
-    public String brand;
-    public String model;
-    public String cpu;
-    public double cpuspeed;
-    public double mem;
-    public double hdd;
-    public double screen; //диагональ экрана
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
 
+class Laptop {
+    Integer sn;
+    String brand;
+    //    private String model;
+//    private String cpu;
+//    private double cpuspeed;
+//    private double mem;
+//    private double hdd;
+//    private double screen; //диагональ экрана
+    Double price;
 
-    public void printlLaptop() {
-        System.out.println("Brand: " + brand + ", model: " + model);
-        System.out.println("CPU: " + cpu + cpuspeed + "GHz");
-        System.out.println("Memory: " + mem + "Gb");
-        System.out.println("HDD: " + hdd + "Gb");
-        System.out.println("Screen: " + screen + '"');
+    void printLP() {
+        System.out.printf("sn: %s, %s, %s \n", sn, brand, price);
     }
 
-    public void addLaptop() {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Добавим новый товар. ");
-        System.out.printf("Введте бренд: ");
-        String brand = iScanner.nextLine();
-        System.out.println("Введите наименование процессора: ");
-        String cpu = iScanner.nextLine();
-        System.out.println("Введите частоту процессора: ");
-        String cpuspeed = iScanner.nextLine();
-        System.out.println("Введите объём оперативной памяти: ");
-        String memory = iScanner.nextLine();
-        System.out.println("Введите объем HDD: ");
-        String hdd = iScanner.nextLine();
-        System.out.println("Введите размер экрана: ");
-        String screen = iScanner.nextLine();
-        iScanner.close();
-
-    }
 }
+
+    //    public String getName() { return name; }
+//    public int getPrice() { return price; }
+//    public String getCompany() { return company; }
+//    public Double getPrice() { return price; }
+//    public Laptop(Integer sn, String brand, Double price) {
+//        this.sn = sn;
+//        this.brand = brand;
+//        this.price = price;
+//    }
+
+
+
+//    public void printlLaptop() {
+//        System.out.println("Laptop. S/N: " + sn + ";");
+////        System.out.println("Brand: " + brand + ", model: " + model);
+////        System.out.println("CPU: " + cpu + ",  " + cpuspeed + "GHz");
+////        System.out.println("Memory: " + mem + "Gb");
+////        System.out.println("HDD: " + hdd + "Gb");
+////        System.out.println("Screen: " + screen + '"');
+//        System.out.println("Price: " + price + "р.");
+//    }
+
+//    public void addLaptop() {
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.printf("Добавим новый товар. ");
+//        System.out.printf("Введте S/N: ");
+//        Integer sn = parseInt(iScanner.nextLine());
+//        this.sn = sn;
+//        System.out.printf("Введте бренд: ");
+//        String brand = iScanner.nextLine();
+//        this.brand = brand;
+////        System.out.println("Введите наименование процессора: ");
+////        String this.cpu = iScanner.nextLine();
+////        System.out.println("Введите частоту процессора: ");
+////        String this.cpuspeed = iScanner.nextLine();
+////        System.out.println("Введите объём оперативной памяти: ");
+////        String this.memory = iScanner.nextLine();
+////        System.out.println("Введите объем HDD: ");
+////        String this.hdd = iScanner.nextLine();
+////        System.out.println("Введите размер экрана: ");
+////        String this.screen = iScanner.nextLine();
+//        System.out.println("Введите цену: ");
+//        this.price = parseDouble(iScanner.nextLine());
+////        iScanner.close();
+//    }
+//    public Double GetPrice() {
+//        return price;
+//    }
+//}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,84 @@
 package org.example;
 
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-        Laptop lt1 = new Laptop();
-
-        System.out.println("Hello world!");
+    Laptop lp1 = new Laptop();
+    lp1.printLP();
+    lp1.sn = 001;
+    lp1.brand = "Asus";
+    lp1.price = 1200.0;
+    lp1.printLP();
     }
 }
+//        Scanner iScanner = new Scanner(System.in);
+//
+//        HashSet<Laptop> laptopset = new HashSet<>();
+//
+//        while(true) {
+//            System.out.println("Команды:");
+//            System.out.println("1 - Добавить ноутбук; \n 2 - Просмотреть все \n 3 - отфильтровать по параметру \n 4 - отсортировать  \n Q - выйти");
+//            String str = iScanner.nextLine();
+//            if (str.equals("Q")) {
+//                iScanner.close();
+//                break;
+//            }
+//            else if (str.equals("1")) {
+//                Laptop lt1 = new Laptop();
+//                lt1.addLaptop();
+//                laptopset.add(lt1);
+//            }
+//            else if (str.equals("2")) PrintAll(laptopset);
+//            else if (str.equals("3")) PrintFiltered(laptopset);
+//
+//            else System.out.println("Неизвестная команда. Повторите ввод.");
+//        }
+//        System.out.println("До свидания!");
+//    }
+//
+//    private static void PrintAll(HashSet<Laptop> laptopset) {
+//        System.out.println("Все ноутбуки:");
+//        for (Laptop lt:laptopset) lt.printlLaptop();
+//    }
+//    private static void PrintFiltered(HashSet<Laptop> laptopset) {
+//        Scanner iScanner = new Scanner(System.in);
+//        while(true) {
+//            System.out.println("Фильтровать по:");
+//            System.out.println("1 - Цена \n 2 - Объём ОЗУ \n 3 - Объём HDD \n 4 - Диагональ экрана  \n Q - назад");
+//            String str = iScanner.nextLine();
+//            if (str.equals("Q")) {
+////                iScanner.close();
+//                break;
+//            }
+//            else if (str.equals("1")) { //Сортировка по Price
+////                Map<Double, String> sortedLTList = new TreeMap<>();
+////                for (Laptop lt:laptopset) {
+////                    sortedLTList.put(lt.price, lt.brand);
+////                    System.out.println("Puted: " + lt.price + lt.brand);
+////                }
+////                System.out.println("Сортировка по цене:");
+////                for (Double key : sortedLTList.keySet()) {
+////                    System.out.println(sortedLTList.get(key) + ",  " + key + "р.\n" );
+////                }
+//                List sortedList = laptopset.stream()
+//                        .sorted(Comparator.comparing(Laptop::getPrice))
+//                        .collect(Collectors.toList());
+//                System.out.println(sortedList);
+//
+//
+//            }
+//
+////            else if (str.equals("2")) PrintAll(laptopset);
+////            else if (str.equals("3")) PrintFiltered(laptopset);
+//
+//            else System.out.println("Неизвестная команда. Повторите ввод. \n");
+//        }
+//
+////        for (Laptop lt:laptopset) lt.printlLaptop();
+//
+//    }
+//
+//}
