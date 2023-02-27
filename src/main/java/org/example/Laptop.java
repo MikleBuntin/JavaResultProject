05ea6b6dc9c99f9aeb7a8aa0ae1c19d3d42c2a40
MikleBuntin <BuntinMA@mail.ru>
2023-02-27 11:01:39 +0300

    Организовал структуру, задействовал конструктор.

---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Laptop {
+    public String brand;
+    public String model;
+    public String cpu;
+    public double cpuspeed;
+    public double mem;
+    public double hdd;
+    public double screen; //диагональ экрана
+
+
+    public void printlLaptop() {
+        System.out.println("Brand: " + brand + ", model: " + model);
+        System.out.println("CPU: " + cpu + cpuspeed + "GHz");
+        System.out.println("Memory: " + mem + "Gb");
+        System.out.println("HDD: " + hdd + "Gb");
+        System.out.println("Screen: " + screen + '"');
+    }
+
+    public void addLaptop() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Добавим новый товар. ");
+        System.out.printf("Введте бренд: ");
+        String brand = iScanner.nextLine();
+        System.out.println("Введите наименование процессора: ");
+        String cpu = iScanner.nextLine();
+        System.out.println("Введите частоту процессора: ");
+        String cpuspeed = iScanner.nextLine();
+        System.out.println("Введите объём оперативной памяти: ");
+        String memory = iScanner.nextLine();
+        System.out.println("Введите объем HDD: ");
+        String hdd = iScanner.nextLine();
+        System.out.println("Введите размер экрана: ");
+        String screen = iScanner.nextLine();
+        iScanner.close();
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,43 +3,11 @@ package org.example;
 
 import java.util.Scanner;
 
-class laptop {
-    public double mem;
-    public double hdd;
-    public double screen; //диагональ экрана
-    public String brand;
-    public String model;
-    public String cpu;
-    public double cpuspeed;
-    public void printlLaptop() {
-        System.out.println("Brand: " + brand + ", model: " + model);
-        System.out.println("CPU: " + cpu + cpuspeed + "GHz");
-        System.out.println("Memory: " + mem + "Gb");
-        System.out.println("HDD: " + hdd + "Gb");
-        System.out.println("Screen: " + screen + '"');
-    }
-
-    public void addLaptop() {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Добавим новый товар. ");
-        System.out.printf("Введте бренд: ");
-        String brand = iScanner.nextLine();
-        System.out.println("Введите наименование процессора: ");
-        String cpu = iScanner.nextLine();
-        System.out.println("Введите частоту процессора: ");
-        String cpuspeed = iScanner.nextLine();
-        System.out.println("Введите объём оперативной памяти: ");
-        String memory = iScanner.nextLine();
-        System.out.println("Введите объем HDD: ");
-        String hdd = iScanner.nextLine();
-        System.out.println("Введите размер экрана: ");
-        String screen = iScanner.nextLine();
-        iScanner.close();
-
-    }
-}
 public class Main {
     public static void main(String[] args) {
+
+        Laptop lt1 = new Laptop();
+
         System.out.println("Hello world!");
     }
 }
