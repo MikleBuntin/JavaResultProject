dd2cfeb226edfbf4fb497a8007d4fcc888e0ba43
MikleBuntin <BuntinMA@mail.ru>
2023-02-27 23:23:30 +0300

    Базовый вариант готов. Хотя, конечно, есть пара моментов и идей для оптимизации. :)

--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -8,50 +8,33 @@ import static java.lang.Integer.parseInt;
 
 class Laptop implements Comparable<Laptop> {
     Integer sn;
-    String brand;
-    //    private String model;
-//    private String cpu;
-//    private double cpuspeed;
-//    private double mem;
+    private String brand;
+
+    private double ram;
     private double hdd;
-//    private double screen; //диагональ экрана
+    private double screen; //диагональ экрана
     Double price;
 
-    void printLP() {
-        System.out.printf("sn: %s, %s, %s \n", sn, brand, price);
-    }
 
+    public String getBrand() {return brand;}
     public Double getPrice() {return price;}
     public Double getHDD() {return hdd;}
+    public Double getRam() {return ram;}
+    public Double getScreen() {return screen;}
 
     @Override
     public int compareTo(Laptop lt) {
         return price.compareTo(lt.getPrice());
     }
 
-
-
-
-    //    public String getName() { return name; }
-//    public int getPrice() { return price; }
-//    public String getCompany() { return company; }
-//    public Double getPrice() { return price; }
-//    public Laptop(Integer sn, String brand, Double price) {
-//        this.sn = sn;
-//        this.brand = brand;
-//        this.price = price;
-//    }
-
-
-
-    public void printlLaptop() {
-        System.out.println("Laptop. S/N: " + sn + ";");
-//        System.out.println("Brand: " + brand + ", model: " + model);
+    public void printLaptop() {
+        System.out.print("Laptop. S/N: " + sn + "; ");
+        System.out.print("Brand: " + brand + ", ");
 //        System.out.println("CPU: " + cpu + ",  " + cpuspeed + "GHz");
-//        System.out.println("Memory: " + mem + "Gb");
-        System.out.println("HDD: " + hdd + "Gb");
-//        System.out.println("Screen: " + screen + '"');
-        System.out.println("Price: " + price + "р.");
+        System.out.print("Memory: " + ram + "Gb; ");
+        System.out.print("HDD: " + hdd + "Gb; ");
+        System.out.print("Screen: " + screen + '"' + "; ");
+        System.out.print("Price: " + price + "р. \n");
     }
 
     public void addLaptop() {
@@ -67,25 +50,24 @@ class Laptop implements Comparable<Laptop> {
 //        String this.cpu = iScanner.nextLine();
 //        System.out.println("Введите частоту процессора: ");
 //        String this.cpuspeed = iScanner.nextLine();
-//        System.out.println("Введите объём оперативной памяти: ");
-//        String this.memory = iScanner.nextLine();
+        System.out.println("Введите объём оперативной памяти: ");
+        this.ram = parseDouble(iScanner.nextLine());
         System.out.println("Введите объем HDD: ");
         this.hdd = parseInt(iScanner.nextLine());
-//        System.out.println("Введите размер экрана: ");
-//        String this.screen = iScanner.nextLine();
+        System.out.println("Введите размер экрана: ");
+        this.screen = parseDouble(iScanner.nextLine());
         System.out.println("Введите цену: ");
         this.price = parseDouble(iScanner.nextLine());
     }
-
-//    @Override
-//    public int compareTo(Laptop o) {
-//        return 0;
-//    }
-//    public Double GetPrice() {
-//        return price;
-//    }
 }
 
+class BrandComparator implements Comparator<Laptop> {
+
+    @Override
+    public int compare(Laptop o1, Laptop o2) {
+        return o1.getBrand().compareTo(o2.getBrand());
+    }
+}
 class PriceComparator implements Comparator<Laptop> {
 
     @Override
@@ -100,4 +82,23 @@ class HDDComparator implements Comparator<Laptop> {
     public int compare(Laptop o1, Laptop o2) {
         return o1.getHDD().compareTo(o2.getHDD());
     }
+
 }
+class RamComparator implements Comparator<Laptop> {
+
+    @Override
+    public int compare(Laptop o1, Laptop o2) {
+        return o1.getRam().compareTo(o2.getRam());
+    }
+}
+
+class ScreenComparator implements Comparator<Laptop> {
+
+    @Override
+    public int compare(Laptop o1, Laptop o2) {
+        return o1.getScreen().compareTo(o2.getScreen());
+    }
+}
+
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,9 @@ package org.example;
 
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-//    Laptop lp1 = new Laptop();
-//    lp1.printLP();
-//    lp1.sn = 001;
-//    lp1.brand = "Asus";
-//    lp1.price = 1200.0;
-//    lp1.printLP();
 
         HashSet<Laptop> laptopHashSet = new HashSet<Laptop>();
 
@@ -23,97 +16,116 @@ public class Main {
             if (str.equals("Q")) {
                 iScanner.close();
                 break;
-            }
-            else if (str.equals("1")) {
+            } else if (str.equals("1")) {
                 Laptop lt1 = new Laptop(); //Создаём пустой экземпляр
-//                Comparator<Laptop> priceComp = new LTPriceComparator();
-//                TreeSet<Laptop> laptopForPrice = new TreeSet(priceComp);
                 lt1.addLaptop(); //Заполняем созданный экземпляр значениями с помощью метода addLaptop()
                 laptopHashSet.add(lt1); // Закидываем текущий экземпляр в HashSet
-            }
+            } else if (str.equals("2")) PrintAll(laptopHashSet);
+            else if (str.equals("3")) PrintFiltered(laptopHashSet);
+            else if (str.equals("4")) PrintSorted(laptopHashSet);
+        }
+    }
 
-            else if (str.equals("2")) {
-//                Laptop lt1 = new Laptop(); //Создаём пустой экземпляр
+    private static void PrintAll(HashSet<Laptop> laptopset) {
+        System.out.println("Все ноутбуки:");
+        for (Laptop lt : laptopset) lt.printLaptop();
+    }
+
+    private static void PrintFiltered(HashSet<Laptop> laptopSet) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Фильтровать по:");
+            System.out.println("1 - Цена \n 2 - Объём ОЗУ \n 3 - Объём HDD \n 4 - Диагональ экрана  \n Q - назад");
+            String str = iScanner.nextLine();
+            if (str.equals("Q")) {
+//                iScanner.close();
+                break;
+            } else if (str.equals("1")) { //Сортировка по Price
                 Comparator<Laptop> priceComp = new PriceComparator();
                 TreeSet<Laptop> laptopForPrice = new TreeSet(priceComp);
-                for (Laptop lt:laptopHashSet) laptopForPrice.add(lt); // Перекидываем все экземпляры из HashSet в TreeSet
-                for (Laptop lt:laptopForPrice) lt.printlLaptop(); // Выводим TreeSet
-            }
-            else if (str.equals("3")) {
+                for (Laptop lt : laptopSet) laptopForPrice.add(lt); // Перекидываем все экземпляры из HashSet в TreeSet
+                System.out.println("Фильтр по размеру экрана: \n");
+                System.out.println("Наименование; цена");
+                for (Laptop lt : laptopForPrice) {
+                    System.out.printf("%s ;  %s \n", lt.getBrand(), lt.getPrice());
+                }
+            } else if (str.equals("2")) { //Сортировка по RAM
+                Comparator<Laptop> ramComp = new RamComparator();
+                TreeSet<Laptop> laptopForRam = new TreeSet(ramComp);
+                for (Laptop lt : laptopSet) laptopForRam.add(lt);
+                System.out.println("Фильтр по размеру RAM: \n");
+                System.out.println("Наименование; объем ОЗУ");
+                for (Laptop lt : laptopForRam) {
+                    System.out.printf("%s ;  %s \n", lt.getBrand(), lt.getRam());
+                }
+            } else if (str.equals("3")) { //Сортировка по HDD
                 Comparator<Laptop> hddComp = new HDDComparator();
                 TreeSet<Laptop> laptopForHDD = new TreeSet(hddComp);
-                for (Laptop lt:laptopHashSet) laptopForHDD.add(lt);
-                for (Laptop lt:laptopForHDD) lt.printlLaptop();
+                for (Laptop lt : laptopSet) laptopForHDD.add(lt);
+                System.out.println("Фильтр по размеру HDD: \n");
+                System.out.println("Наименование; объём диска");
+                for (Laptop lt : laptopForHDD) {
+                    System.out.printf("%s ;  %s \n", lt.getBrand(), lt.getHDD());
+                }
+            } else if (str.equals("4")) { //Сортировка по Screen
+                Comparator<Laptop> screenComp = new ScreenComparator();
+                TreeSet<Laptop> laptopForScreen = new TreeSet(screenComp);
+                for (Laptop lt : laptopSet) laptopForScreen.add(lt);
+                System.out.println("Фильтр по размеру экрана: \n");
+                System.out.println("Наименование; диагональ");
+                for (Laptop lt : laptopForScreen) {
+                    System.out.printf("%s ;  %s \n", lt.getBrand(), lt.getScreen());
+                }
+            } else System.out.println("Неизвестная команда. Повторите ввод. \n");
+        }
+    }
+
+    private static void PrintSorted(HashSet<Laptop> laptopSet) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Сортировать по:");
+            System.out.println(" 1 - Название \n 2 - Объём ОЗУ \n 3 - Объём HDD \n 4 - Диагональ экрана  \n 5 - Цена  \n Q - назад");
+            String str = iScanner.nextLine();
+            if (str.equals("Q")) {
+                break;
             }
 
+            else if (str.equals("1")) { //Сортировка по Brand
+                Comparator<Laptop> brandComp = new BrandComparator();
+                TreeSet<Laptop> laptopForBrand = new TreeSet(brandComp);
+                for (Laptop lt : laptopSet) laptopForBrand.add(lt);
+                System.out.println("Сортировка по названию: \n");
+                for (Laptop lt : laptopForBrand) lt.printLaptop();
+            }
+            else if (str.equals("2")) { //Сортировка по ОЗУ
+                Comparator<Laptop> ramComp = new RamComparator();
+                TreeSet<Laptop> laptopForRam = new TreeSet(ramComp);
+                for (Laptop lt : laptopSet) laptopForRam.add(lt);
+                System.out.println("Сортировка по RAM: \n");
+                for (Laptop lt : laptopForRam) lt.printLaptop();
+            }
+            else if (str.equals("3")) { //Сортировка по HDD
+                Comparator<Laptop> hddComp = new HDDComparator();
+                TreeSet<Laptop> laptopForHdd = new TreeSet(hddComp);
+                for (Laptop lt : laptopSet) laptopForHdd.add(lt);
+                System.out.println("Сортировка по HDD: \n");
+                for (Laptop lt : laptopForHdd) lt.printLaptop();
+            }
+            else if (str.equals("4")) { //Сортировка по Screen
+                Comparator<Laptop> screenComp = new ScreenComparator();
+                TreeSet<Laptop> laptopForScreen = new TreeSet(screenComp);
+                for (Laptop lt : laptopSet) laptopForScreen.add(lt);
+                System.out.println("Сортировка по названию: \n");
+                for (Laptop lt : laptopForScreen) lt.printLaptop();
+            }
+            else if (str.equals("5")) { //Сортировка по Price
+                Comparator<Laptop> priceComp = new PriceComparator();
+                TreeSet<Laptop> laptopForPrice = new TreeSet(priceComp);
+                for (Laptop lt : laptopSet) laptopForPrice.add(lt); // Перекидываем все экземпляры из HashSet в TreeSet
+                System.out.println("Сортировка по цене: \n");
+                for (Laptop lt : laptopForPrice) lt.printLaptop();
+            }
+            else System.out.println("Неизвестная команда. Повторите ввод. \n");
         }
     }
 }
-//        Scanner iScanner = new Scanner(System.in);
-//
-//        HashSet<Laptop> laptopset = new HashSet<>();
-//
-//        while(true) {
-//            System.out.println("Команды:");
-//            System.out.println("1 - Добавить ноутбук; \n 2 - Просмотреть все \n 3 - отфильтровать по параметру \n 4 - отсортировать  \n Q - выйти");
-//            String str = iScanner.nextLine();
-//            if (str.equals("Q")) {
-//                iScanner.close();
-//                break;
-//            }
-//            else if (str.equals("1")) {
-//                Laptop lt1 = new Laptop();
-//                lt1.addLaptop();
-//                laptopset.add(lt1);
-//            }
-//            else if (str.equals("2")) PrintAll(laptopset);
-//            else if (str.equals("3")) PrintFiltered(laptopset);
-//
-//            else System.out.println("Неизвестная команда. Повторите ввод.");
-//        }
-//        System.out.println("До свидания!");
-//    }
-//
-//    private static void PrintAll(HashSet<Laptop> laptopset) {
-//        System.out.println("Все ноутбуки:");
-//        for (Laptop lt:laptopset) lt.printlLaptop();
-//    }
-//    private static void PrintFiltered(HashSet<Laptop> laptopset) {
-//        Scanner iScanner = new Scanner(System.in);
-//        while(true) {
-//            System.out.println("Фильтровать по:");
-//            System.out.println("1 - Цена \n 2 - Объём ОЗУ \n 3 - Объём HDD \n 4 - Диагональ экрана  \n Q - назад");
-//            String str = iScanner.nextLine();
-//            if (str.equals("Q")) {
-////                iScanner.close();
-//                break;
-//            }
-//            else if (str.equals("1")) { //Сортировка по Price
-////                Map<Double, String> sortedLTList = new TreeMap<>();
-////                for (Laptop lt:laptopset) {
-////                    sortedLTList.put(lt.price, lt.brand);
-////                    System.out.println("Puted: " + lt.price + lt.brand);
-////                }
-////                System.out.println("Сортировка по цене:");
-////                for (Double key : sortedLTList.keySet()) {
-////                    System.out.println(sortedLTList.get(key) + ",  " + key + "р.\n" );
-////                }
-//                List sortedList = laptopset.stream()
-//                        .sorted(Comparator.comparing(Laptop::getPrice))
-//                        .collect(Collectors.toList());
-//                System.out.println(sortedList);
-//
-//
-//            }
-//
-////            else if (str.equals("2")) PrintAll(laptopset);
-////            else if (str.equals("3")) PrintFiltered(laptopset);
-//
-//            else System.out.println("Неизвестная команда. Повторите ввод. \n");
-//        }
-//
-////        for (Laptop lt:laptopset) lt.printlLaptop();
-//
-//    }
-//
-//}
